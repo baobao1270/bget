5896cea905646dfed93d581d10fae7c9cde650a4
Joseph Chris <joseph@josephcz.xyz>
2022-01-25 11:28:25 -0800

    update to 3.2.0; sync version number with bgetlib

--- bgetcli/bget.py
+++ bgetcli/bget.py
@@ -1,142 +1,188 @@
-import os
-import sys
-import time
-import toml
 import argparse
-import traceback
-from datetime import datetime
-from typing import List, Tuple
-from bgetlib.model import FavoriteItem, VideoWithPart
+from typing import List, Optional
 
-from .downloader import download_danmaku, download_cover, download_meta, download_parts
-from .model import Runtime
-from .utils import read_config, abspath, outdirs_init
-from .logger import Logger
-from .const import VERSION, TIME_FORMATTER
+from .utils import auto_format
+from .runtime import Config, Runtime, SectionHead
+from . import downloader
+
+
+VERSION = "3.2.0"
+DEFAULT_CONFIG = Config(
+    outdir=".",
+    cookies="bilibili.com_cookies.txt",
+    chunk_size=8192,
+    host=None,
+    formatter={
+        "audio": "av{aid}-{p:0>3d}-{title}.{ext}",
+        "video": "av{aid}-{p:0>3d}-{title}.mp4",
+        "cover": "av{aid}.{ext}",
+        "danmaku": "av{aid}-{cid}.xml",
+        "meta": "av{aid}.json",
+    },
+    switches=["video", "danmaku", "meta"]
+)
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(prog="bget", usage="bget [OPTIONS] <section>",
+    parser = argparse.ArgumentParser(prog="bget", usage="bget [OPTIONS] <resource>",
                                      description="bget - a python bilibili favourites batch downloader",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("alias", metavar="<section>", type=str,
-                        help="section name of favourite in config file")
-    parser.add_argument("-s", "--skip", metavar="<offset>", type=int, default=0,
-                        help="skip <offset> videos form start")
-    parser.add_argument("-k", "--cookies", metavar="<cookies>", type=str, default="bilibili.com_cookies.txt",
-                        help="cookies file path")
-    parser.add_argument("-c", "--config", metavar="<config>", type=str, default="config.toml",
-                        help="config file path")
-    parser.add_argument("-t", "--head", metavar="<head>", type=str, default="head.toml",
-                        help="head file path")
-    parser.add_argument("-o", "--out", metavar="<output-folder>", type=str, default="downloads",
-                        help="download output folder path")
+    parser.add_argument("resource", metavar="<resource>", type=str,
+                        help="a bilibili resource, url, video id or section name")
     parser.add_argument("-v", "--version", action="version", version=VERSION)
+
+    # common settings
+    parser.add_argument("--config", metavar="<config>", type=str, default=None,
+                        help="config file path")
+    parser.add_argument("-k", "--cookies", metavar="<cookies>", type=str, default=None,
+                        help="cookies file path")
+    parser.add_argument("-o", "--outdir", action="store", metavar="<outdir>", type=str, default=None,
+                        help="download output folder path")
+    parser.add_argument("--chunk-size", metavar="<chunk-size-byte>", type=int, default=None,
+                        help="chunk size of downloader")
+    parser.add_argument("--host", metavar="<host>", type=str, default=None,
+                        help="override the host of stream CDN")
+    parser.add_argument("--no-meta", action="store_true", help="do not download metadata")
+    parser.add_argument("--no-danmaku", action="store_true", help="do not download danmaku")
+    parser.add_argument("--audio-only", action="store_true", help="download audio only")
+    parser.add_argument("--with-cover", action="store_true", help="download cover picture")
+
+    # Section Mode
+    parser.add_argument("-s", "--skip", metavar="<offset>", type=int, default=0,
+                        help="skip <offset> videos if downloading a list, not work for downloading single video")
+    parser.add_argument("--section", action="store_true", help="enable section mode")
+    parser.add_argument("--section-head", metavar="<head>", type=str, default="head.json",
+                        help="section mode: path to head")
     return parser.parse_args()
-
-
-def skip(rt: Runtime, collection_videos: List[FavoriteItem]):
-    rt.logger.push("skip")
-    skipped = collection_videos[:rt.skip]
-    not_skipped = collection_videos[rt.skip:]
-    for v in skipped:
-        rt.logger.log("av{}: {:20}".format(v.avid, v.title))
-    rt.logger.pop()
-    return not_skipped
-
-
-def ensure_dirs_created(rt: Runtime):
-    os.makedirs(rt.config.cache_dir, exist_ok=True)
-    # noinspection PyPep8Naming,SpellCheckingInspection
-    OUTDIR = outdirs_init(rt)
-    os.makedirs(OUTDIR.DASH, exist_ok=True)
-    if rt.config.folder.flac_path is not None:
-        os.makedirs(OUTDIR.FLAC, exist_ok=True)
-    if rt.config.folder.aiff_path is not None:
-        os.makedirs(OUTDIR.AIFF, exist_ok=True)
-    if rt.config.folder.danmaku_path is not None:
-        os.makedirs(OUTDIR.DANMAKU, exist_ok=True)
-    if rt.config.folder.cover_path is not None:
-        os.makedirs(OUTDIR.COVER, exist_ok=True)
-    if rt.config.folder.meta_path is not None:
-        os.makedirs(OUTDIR.META, exist_ok=True)
-
-
-def get_pending_download(rt: Runtime) -> Tuple[List[FavoriteItem], datetime]:
-    rt.logger.log("Using collection '{}' (id={})".format(rt.config.name, rt.config.id))
-    rt.logger.log("HEAD is {}".format(rt.head.strftime(TIME_FORMATTER)))
-    collection_videos = rt.api.get_favorites_nbf(favorite_id=rt.config.id,
-                                                 from_timestamp=int(rt.head.timestamp()))
-    snapshot_time = datetime.fromtimestamp(int(datetime.now().timestamp()))
-    collection_videos = skip(rt, collection_videos)
-    return collection_videos, snapshot_time
-
-
-def download_collection(rt: Runtime, collection_videos: List[FavoriteItem], multipart_videos: List[VideoWithPart]):
-    for collection_item in collection_videos:
-        video = rt.api.get_video(collection_item.avid)
-        rt.logger.push("av{}".format(video.avid))
-        rt.logger.log("Title {:40}".format(video.title))
-        rt.logger.log("Parts: {}, Uploader: {} (uid={})".format(
-            len(video.parts), video.uploader.name, video.uploader.uid))
-        download_video(rt, video)
-        if len(video.parts) > 1:
-            multipart_videos.append(video)
-        rt.logger.pop()
-
-
-def download_video(rt: Runtime, video: VideoWithPart):
-    if rt.config.folder.danmaku_path is not None:
-        download_danmaku(rt, video)
-    if rt.config.folder.cover_path is not None:
-        download_cover(rt, video)
-    if rt.config.folder.meta_path is not None:
-        download_meta(rt, video)
-    download_parts(rt, video)
-    time.sleep(5)
-
-
-def update_head(rt: Runtime, old_head: datetime, new_head: datetime):
-    head_filepath = abspath(rt.args.head)
-    heads = {}
-    if os.path.exists(head_filepath):
-        heads = toml.load(head_filepath)
-    heads[rt.config.name] = new_head
-    with open(head_filepath, "w+", encoding="utf-8") as f:
-        toml.dump(heads, f)
-    rt.logger.tags = []
-    rt.logger.log("New HEAD is set: {} -> {}".format(
-        old_head.strftime(TIME_FORMATTER), new_head.strftime(TIME_FORMATTER)))
-
-
-def print_multipart_videos(rt: Runtime, multipart_videos: List[VideoWithPart]):
-    if len(multipart_videos) == 0:
-        return
-    rt.logger.tags = []
-    rt.logger.log("Multipart Videos:")
-    for v in multipart_videos:
-        rt.logger.log("\tav{}\t{}".format(v.avid, v.title))
-        rt.logger.log("\t\tLink: https://b23.tv/av{}".format(v.avid))
-        for p in v.parts:
-            rt.logger.log("\t\tcid={} {}".format(p.cid, p.name))
 
 
 def main():
     args = parse_args()
-    logger = Logger()
-    config, head = read_config(logger, args)
-    runtime = Runtime(args=args, config=config, head=head, logger=logger)
-    ensure_dirs_created(runtime)
-    collection_videos, snapshot_time = get_pending_download(runtime)
-    multipart_videos: List[VideoWithPart] = []
-    # noinspection PyBroadException
-    try:
-        download_collection(runtime, collection_videos, multipart_videos)
-        update_head(runtime, head, snapshot_time)
-    except:
-        traceback.print_exc()
-    finally:
-        print_multipart_videos(runtime, multipart_videos)
+    runtime = Runtime.factory(args, DEFAULT_CONFIG)
+    section_head = SectionHead(runtime)
+    download_tasks = generate_tasks(runtime, section_head)
+    section_head.tick()
+
+    videos = checkout_videos(runtime, download_tasks)
+    download_videos(runtime, videos)
+
+    section_head.write()
+    report(runtime)
+    runtime.log("All done.")
+
+
+def logger_tag(tag: str):
+    def decorator(func):
+        def wrapper(runtime: Runtime, *values, **kwargs):
+            runtime.log_tags.append(tag)
+            result = func(runtime, *values, **kwargs)
+            runtime.log_tags.pop()
+            return result
+        return wrapper
+    return decorator
+
+
+def generate_tasks(runtime: Runtime, section_head: SectionHead):
+    tasks = list()
+    if runtime.resource_type == "fav":
+        if runtime.section_name is None:
+            tasks = runtime.bapi.get_favorites_all(runtime.resource_id)
+        else:
+            tasks = runtime.bapi.get_favorites_since(runtime.resource_id, section_head.read())
+        tasks = skip(runtime, tasks, runtime.args.skip)
+    if runtime.resource_type == "video":
+        tasks = [{"id": runtime.resource_id}]
+    return tasks
+
+
+@logger_tag("skip")
+def skip(runtime: Runtime, tasks: List[dict], skip_count: int) -> List[dict]:
+    if skip_count == 0:
+        return tasks
+    runtime.log(f"Skipping {skip_count} items below:")
+    skipped = tasks[:skip_count]
+    remains = tasks[skip_count:]
+    runtime.report.skip = skipped
+    for task in skipped:
+        runtime.log(f"Skip: av{task['id']}: {task['title']:20}")
+    return remains
+
+
+@logger_tag("checkout")
+def checkout_videos(runtime: Runtime, tasks: List) -> List[dict]:
+    videos = list()
+    for i in range(len(tasks)):
+        task = tasks[i]
+        aid = task["id"]
+        # noinspection PyBroadException
+        try:
+            video = runtime.bapi.get_video(aid)
+        except:
+            runtime.report.inaccessible.append(task)
+            runtime.log(f"Inaccessible: av{aid}: {task['title']:20}")
+            continue
+        videos.append(video)
+    runtime.log(f"Checkout {len(videos)} videos, {len(runtime.report.inaccessible)} inaccessible.")
+    return videos
+
+
+@logger_tag("download")
+def download_videos(runtime: Runtime, videos: List):
+    for i in range(len(videos)):
+        video = videos[i]
+        if len(video["pages"]) > 1:
+            runtime.report.multipart.append(video)
+        runtime.log(f"Start downloading {i+1}/{len(videos)} https://b23.tv/av{video['aid']}")
+        runtime.log_tags.append(f"av{video['aid']}")
+        download_video(runtime, video)
+        runtime.log_tags.pop()
+
+
+def download_video(runtime: Runtime, video: dict):
+    runtime.log(auto_format("av{aid} {bvid} {parts}P up='{up}' (uid={up_uid})", video))
+    runtime.log(auto_format("Title: {title}", video))
+    for p in range(len(video["pages"])):
+        runtime.log(auto_format("Part {p}/{parts} cid={cid}: {part_name}", video, p))
+        if "audio" in runtime.config.switches:
+            downloader.download_audio(runtime, video, p)
+        if "video" in runtime.config.switches:
+            downloader.download_video(runtime, video, p)
+        if "danmaku" in runtime.config.switches:
+            downloader.download_danmaku(runtime, video, p)
+    if "cover" in runtime.config.switches:
+        downloader.download_cover(runtime, video)
+    if "meta" in runtime.config.switches:
+        downloader.download_meta(runtime, video)
+    runtime.log(f"Done download av{video['aid']}")
+
+
+def report(runtime: Runtime):
+    print("=" * 80)
+    print(f"""Download Report
+    Skip: {len(runtime.report.skip)}
+    Inaccessible: {len(runtime.report.inaccessible)}
+    Error: {len(runtime.report.error)}
+    Multipart: {len(runtime.report.multipart)}
+    """)
+
+    print("\n====== Skip ========")
+    for task in runtime.report.skip:
+        print(f"https://b23.tv/av{task['id']:<20} {task['title']:20}")
+
+    print("\n=== Inaccessible ===")
+    for task in runtime.report.inaccessible:
+        print(f"https://b23.tv/av{task['id']:<20} {task['title']:20}")
+
+    # print("\n====== Error =======")
+    print("\n==== Multipart =====")
+    for video in runtime.report.multipart:
+        print(f"https://b23.tv/av{video['aid']:<20} {len(video['pages'])}P {video['title']:16}")
+        for p in range(len(video["pages"])):
+            part = video["pages"][p]
+            print(f"      P{p+1:<5} cid={part['cid']:<15} {part['part']:20}")
+
+    print("\n\nEnd of Download Report")
+    print("=" * 80, "\n")
 
 
 if __name__ == "__main__":
---
+++ bgetcli/runtime.py
@@ -0,0 +1,160 @@
+import json
+import os
+import sys
+import time
+import toml
+import bgetlib
+import argparse
+from dataclasses import dataclass, field
+from typing import Optional, Dict, List, Tuple
+from .utils import list_unique, parse_resources
+
+
+@dataclass
+class Report:
+    skip: List[Dict] = field(default_factory=list)
+    error: List[Dict] = field(default_factory=list)
+    multipart: List[Dict] = field(default_factory=list)
+    inaccessible: List[Dict] = field(default_factory=list)
+
+
+@dataclass
+class Config:
+    outdir: str
+    cookies: str
+    chunk_size: int
+    host: Optional[str]
+    switches: List[str]
+    formatter: Dict[str, str]
+
+    def load(self, raw_config: dict, section_name: Optional[str] = None):
+        override_config = raw_config
+        if section_name is not None:
+            override_config = raw_config.get("section", {}).get(section_name, {})
+        self.outdir = override_config.get("outdir", self.outdir)
+        self.cookies = override_config.get("cookies", self.cookies)
+        self.chunk_size = override_config.get("chunk-size", self.chunk_size)
+        self.host = override_config.get("host", self.host)
+        self.switches = list_unique(override_config.get("switches", self.switches))
+        for key in ["meta", "audio", "video", "cover", "meta"]:
+            self.formatter[key] = override_config.get("formatter-" + key, self.formatter[key])
+
+    def load_args(self, args: argparse.Namespace):
+        self.cookies = args.cookies or self.cookies
+        self.outdir = args.outdir or self.outdir
+        self.chunk_size = args.chunk_size or self.chunk_size
+        self.host = args.host or self.host
+        if args.no_meta:
+            self.switches = list_unique(self.switches)
+            self.switches.remove("meta")
+        if args.no_danmaku:
+            self.switches = list_unique(self.switches)
+            self.switches.remove("danmaku")
+        if args.with_cover:
+            self.switches = list_unique(self.switches)
+            self.switches.append("danmaku")
+        if args.audio_only:
+            self.switches.append("audio")
+            self.switches = list_unique(self.switches)
+            self.switches.remove("video")
+
+    def print_self(self, logger):
+        logger(
+            "Calculated configuration:",
+            f"dest={self.outdir}",
+            f"cookies={self.cookies}",
+            f"chunk_size={self.chunk_size}",
+            f"host={self.host}"
+        )
+        logger(f"Switches: {', '.join(self.switches)}")
+        logger(f"Formatters:")
+        for key, value in self.formatter.items():
+            logger(f"  {key}: {value}")
+
+
+@dataclass
+class Runtime:
+    bapi: bgetlib.BilibiliAPI
+    args: argparse.Namespace
+    report: Report
+    config: Config
+    section_name: str
+    resource_type: str
+    resource_id: int
+    log_tags: List[str] = field(init=False, default_factory=list)
+
+    def __post_init__(self):
+        if self.resource_type == "notfound":
+            self.log("Section name not found in configuration")
+            sys.exit()
+        if self.resource_type == "unknown":
+            self.log("Can not parse resource")
+            sys.exit()
+        self.config.print_self(self.log)
+        self.log(f"Fetching resource {self.resource_type}:{self.resource_id}")
+
+    @staticmethod
+    def factory(args: argparse.Namespace, config: Config):
+        section_name: str = args.resource.strip() if args.section else None
+        config_dict = toml.load(args.config) if args.config is not None else {"section": {}}
+        config = Runtime.build_config(args, config, section_name)
+        resource_type, resource_id = parse_resources(args, config_dict)
+        bapi = bgetlib.BilibiliAPI(config.cookies)
+        report = Report()
+        return Runtime(bapi, args, report, config, section_name, resource_type, resource_id)
+
+    @staticmethod
+    def build_config(args: argparse.Namespace, config: Config, section_name: str) -> Config:
+        config_dict = toml.load(args.config) if args.config is not None else {"section": {}}
+        config.load(config_dict)
+        config.load_args(args)
+        if args.section:
+            config.load(config_dict, section_name)
+        return config
+
+    def log(self, *values, **kwargs):
+        tags = "".join([f"[{tag}]" for tag in self.log_tags])
+        print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}]" + tags, end="")
+        print(*values, **kwargs)
+
+    def log_progress(self, *values, **kwargs):
+        kwargs["end"] = "\r"
+        kwargs["flush"] = True
+        self.log(*values, **kwargs)
+
+
+class SectionHead:
+    def __init__(self, runtime: Runtime):
+        self.runtime: Runtime = runtime
+        self.section: str = runtime.section_name
+        self.filename: str = runtime.args.section_head
+        self.tick_at: Optional[int] = None
+
+    def tick(self):
+        self.tick_at = int(time.time())
+        self.runtime.log(f"Start downloading at {self.tick_at}")
+
+    def read(self) -> int:
+        if (not os.path.exists(self.filename)) or (self.runtime.section_name is None):
+            return 172800  # offset 2 day to avoid timezone issue
+        with open(self.filename, "r", encoding="utf-8") as f:
+            head_timestamp = json.load(f).get(self.runtime.section_name)
+        head_timestamp = head_timestamp or 172800
+        self.runtime.log("Reading head timestamp: {}")
+        return head_timestamp or 172800
+
+    def write(self):
+        if self.runtime.section_name is None:
+            return
+        if self.tick_at is None:
+            self.runtime.log("Error: can not write before tick")
+            sys.exit()
+        if not os.path.exists(self.filename):
+            heads = {}
+        else:
+            with open(self.filename, "r", encoding="utf-8") as f:
+                heads = json.load(f)
+        self.runtime.log(f"Writing head timestamp: {self.tick_at}")
+        heads[self.runtime.section_name] = self.tick_at
+        with open(self.filename, "w+", encoding="utf-8") as f:
+            f.write(json.dumps(heads, ensure_ascii=False, indent=4))
